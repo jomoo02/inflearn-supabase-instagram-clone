93c0a0b52b5e3199849bb5b599eacd7166db510d
jomoo02 <jomootgrn88@naver.com>
2025-03-28 20:05:13 +0900

    Part 8 - 실시간 채팅, 유저별 접속시간

--- components/chat/chat-pepole-list.tsx
+++ components/chat/chat-pepole-list.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Person from "./person";
 import { useRecoilState } from "recoil";
 import {
   selectedUserIdState,
   selectedUserIndexState,
+  presenceState,
 } from "utils/recoil/atoms";
 import { getAllUsers } from "actions/chat-actions";
 import { useQuery } from "@tanstack/react-query";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function ChatPeopleList({ loggedInUser }) {
   const [selectedUserId, setSelectedUserId] =
@@ -17,6 +19,8 @@ export default function ChatPeopleList({ loggedInUser }) {
   const [selectedUserIndex, setSelectedUserIndex] = useRecoilState(
     selectedUserIndexState
   );
+
+  const [presence, setPresence] = useRecoilState(presenceState);
 
   const getAllUsersQuery = useQuery({
     queryKey: ["users"],
@@ -27,6 +31,44 @@ export default function ChatPeopleList({ loggedInUser }) {
     },
   });
 
+  const supabase = createBrowserSupabaseClient();
+
+  useEffect(() => {
+    const channel = supabase.channel("online_users", {
+      config: {
+        presence: {
+          key: loggedInUser.id,
+        },
+      },
+    });
+
+    channel.on(
+      "presence",
+      {
+        event: "sync",
+      },
+      () => {
+        const newState = channel.presenceState();
+        const newStateObj = JSON.parse(JSON.stringify(newState));
+        setPresence(newStateObj);
+      }
+    );
+
+    channel.subscribe(async (status) => {
+      if (status !== "SUBSCRIBED") {
+        return;
+      }
+
+      const newPresenceStatus = await channel.track({
+        onlineAt: new Date().toISOString(),
+      });
+    });
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
+
   return (
     <div className="h-screen w-60 flex flex-col bg-gray-50">
       {getAllUsersQuery.data?.map((user, index) => (
@@ -36,7 +78,7 @@ export default function ChatPeopleList({ loggedInUser }) {
           isActive={selectedUserId === user.id}
           name={user.email.split("@")[0]}
           onChatScreen={false}
-          onlineAt={new Date().toISOString()}
+          onlineAt={presence?.[user.id]?.[0]?.onlineAt}
           userId={user.id}
           onClick={() => {
             setSelectedUserId(user.id);
--- components/chat/chat-screen.tsx
+++ components/chat/chat-screen.tsx
@@ -4,20 +4,68 @@ import Person from "./person";
 import Message from "./message";
 import { useRecoilValue } from "recoil";
 import {
+  presenceState,
   selectedUserIdState,
   selectedUserIndexState,
 } from "utils/recoil/atoms";
-import { useQuery } from "@tanstack/react-query";
-import { getUserById } from "actions/chat-actions";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getUserById, sendMessage, getAllMessages } from "actions/chat-actions";
+import { useEffect, useState } from "react";
+import { Spinner } from "@material-tailwind/react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function ChatScreen() {
   const selectedUserId = useRecoilValue(selectedUserIdState);
   const selectedUserIndex = useRecoilValue(selectedUserIndexState);
+  const presence = useRecoilValue(presenceState);
+
+  const [message, setMessage] = useState("");
+
+  const supabase = createBrowserSupabaseClient();
 
   const selectedUserQuery = useQuery({
     queryKey: ["user", selectedUserId],
     queryFn: () => getUserById(selectedUserId),
   });
+
+  const sendMessagesMutation = useMutation({
+    mutationFn: async () => {
+      return sendMessage({ message, chatUserId: selectedUserId });
+    },
+    onSuccess: () => {
+      setMessage("");
+      getAllMessagesQuery.refetch();
+    },
+  });
+
+  const getAllMessagesQuery = useQuery({
+    queryKey: ["messages", selectedUserId],
+    queryFn: () => getAllMessages({ chatUserId: selectedUserId }),
+  });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("message_postgres_changes")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "message" },
+        (payload) => {
+          if (
+            payload.eventType === "INSERT" &&
+            !payload.errors &&
+            !!payload.new
+          ) {
+            getAllMessagesQuery.refetch();
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
+
   return selectedUserQuery.data !== null ? (
     <div className="w-full h-screen flex flex-col">
       <Person
@@ -25,20 +73,30 @@ export default function ChatScreen() {
         isActive={false}
         name={selectedUserQuery.data?.email?.split("@")?.[0]}
         onChatScreen={true}
-        onlineAt={new Date().toISOString()}
+        onlineAt={presence?.[selectedUserId]?.[0]?.onlineAt}
         userId={selectedUserQuery.data?.id}
       />
-      <div className="w-full flex-1 bg-white flex flex-col p-4 gap-3">
-        <Message isFromMe={true} message={"teetetetet"} />
-        <Message isFromMe={false} message={"teetetetet"} />
+      <div className="w-full overflow-y-scroll flex-1 bg-white flex flex-col p-4 gap-3">
+        {getAllMessagesQuery.data?.map((message) => (
+          <Message
+            key={message.id}
+            message={message.message}
+            isFromMe={message.receiver === selectedUserId}
+          />
+        ))}
       </div>
       <div className="flex">
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className="p-2.5 w-full border-2 border-light-blue-600"
           placeholder="메시지를 입력하세요."
         />
-        <button className="min-w-20 p-1 bg-light-blue-600 text-white">
-          <span>전송</span>
+        <button
+          onClick={() => sendMessagesMutation.mutate()}
+          className="min-w-20 p-1 bg-light-blue-600 text-white flex justify-center items-center"
+        >
+          {sendMessagesMutation.isPending ? <Spinner /> : <span>전송</span>}
         </button>
       </div>
     </div>
